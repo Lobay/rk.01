02e075ef9a7f0c7367ba92f87233318a2933d883
Lobay <32384708+Lobay@users.noreply.github.com>
2017-11-11 23:06:53 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,2 +1,19 @@
 #include<iostream>
-using namecpase std;
+using namespace std;
+int main(){
+    int a, i;
+    cin >> a;
+    if (a <= 0)
+        return 0;
+    int k[a];
+    for (i = 0; i < a; i++) {
+        cin >> k[i];
+    }
+    for (i = 0; i < a / 2; i++) {
+        swap(k[i], k[a - i - 1]);
+    }
+    for (i = 0; i < a; i++) {
+        cout << k[i] << " ";
+    }
+    return 0;
+}
