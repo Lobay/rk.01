7f92f816a09282b9960bc428995040b813c28aef
Lobay <32384708+Lobay@users.noreply.github.com>
2017-11-13 15:02:08 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,19 +1,21 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
-    int a, i;
-    cin >> a;
-    if (a <= 0)
-        return 0;
-    int k[a];
-    for (i = 0; i < a; i++) {
-        cin >> k[i];
-    }
-    for (i = 0; i < a / 2; i++) {
-        swap(k[i], k[a - i - 1]);
-    }
-    for (i = 0; i < a; i++) {
-        cout << k[i] << " ";
-    }
-    return 0;
+int main(int argc, char* argv[]){
+ int a, i;
+ cin >> a;
+ if (a <= 0){
+ cout << "An error has occurred while reading input data" << endl;
+ return 0; 
+ }
+ int *k = new int[a];
+ for (i = 0; i < a; i++) {
+ cin >> k[i];
+ }
+ for (i = 0; i < a / 2; i++) {
+ swap(k[i], k[a - i - 1]);
+ }
+ for (i = 0; i < a; i++) {
+ cout << k[i] << " ";
+ }
+ return 0;
 }
